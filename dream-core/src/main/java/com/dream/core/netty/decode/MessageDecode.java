52b3630fa14e5f146ab5d48c4c52146660299d33
Dream <mengyu.jin@nx-engine.com>
2019-06-04 13:26:21 +0800

    lengthAdjustment:数据(body)长度字段之后的字节数

--- dream-core/src/main/java/com/dream/core/netty/decode/MessageDecode.java
+++ dream-core/src/main/java/com/dream/core/netty/decode/MessageDecode.java
@@ -15,7 +15,7 @@ public class MessageDecode extends LengthFieldBasedFrameDecoder {
      * @param maxFrameLength 单个包最大的长度，这个值根据实际场景而定
      * @param lengthFieldOffset 数据长度字段开始的偏移量(下标)
      * @param lengthFieldLength 数据长度字段的所占的字节数
-     * @param lengthAdjustment （lengthAdjustment + 数据长度取值 = 数据(body)长度字段之后剩下包的字节数）
+     * @param lengthAdjustment 数据(body)长度字段之后剩下包的字节数
      * @param initialBytesToStrip 表示从整个包第一个字节开始，向后忽略的字节数
      * @param failFast 为true，当frame长度超过maxFrameLength时立即报TooLongFrameException异常，为false，读取完整个帧再报异
      */
--- dream-gateway-tcu/src/main/java/com/dream/netty/TcuHandler.java
+++ dream-gateway-tcu/src/main/java/com/dream/netty/TcuHandler.java
@@ -70,22 +70,27 @@ public class TcuHandler extends ChannelInboundHandlerAdapter {
         byte[] commonHead = new byte[24];
         commonHead[0]=(byte)0x23;
         commonHead[1]=(byte)0x23;
-        commonHead[2]=(byte)0x07;//心跳指令
-        commonHead[3]=(byte)0xFE;
+        commonHead[2]=(byte)0x01;//心跳指令
+        commonHead[3]=(byte)0x02;
         byte[] vinBytes = "ABCDE600000000009".getBytes();
         for (int i = 0; i < vinBytes.length ; i++) {
             commonHead[i+4] = vinBytes[i];
         }
-        commonHead[21]= (byte)0x01;
+        commonHead[21]= (byte)0x03;
         byte[] bytes = ByteUtil.shortToBytes((short) 0);
-        commonHead[22]=bytes[0];
-        commonHead[23]=bytes[1];
+        commonHead[22]=bytes[0]; //数据长度
+        commonHead[23]=bytes[1]; //数据长度
         byte[] heartBytes = addCommonTail(commonHead);
         ByteBuf byteBuf = Unpooled.buffer();
         byteBuf.order(ByteOrder.BIG_ENDIAN);
         byteBuf.writeBytes(heartBytes);
         LOGGER.info(ByteUtil.bytesToHexString(ByteUtil.decodeValue(byteBuf.nioBuffer())));
         return byteBuf;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( String.format("%04x",Integer.parseInt("65531")));
+
     }
 
     /**
